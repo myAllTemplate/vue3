7557b02e2819a2660e213167a15cd9f1b5906223
wuwuwu <2624086659@qq.com>
2023-06-04 16:21:21 +0800

    改第一次得bug

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,11 @@ import 'normalize.css'
 import '@/assets/css/index.less'
 import App from './App.vue'
 import router from './router'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import { useLoginStore } from './stores/login/login'
-import { localCache } from './utils/cache'
-import { LOGIN_TOKEN } from './global/constants'
 
 // import './assets/main.css'
 
 const app = createApp(App)
 app.use(createPinia())
-
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
-}
-
-const loginStore = useLoginStore()
-loginStore.loadLocalCacheAction()
 
 app.use(router)
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
@@ -18,15 +14,7 @@ export default defineConfig({
       }
     }
   },
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()]
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()]
-    })
-  ],
+  plugins: [vue()],
 
   resolve: {
     alias: {
